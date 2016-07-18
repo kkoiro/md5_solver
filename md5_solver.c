65a0798ec35a8758d17e04d2277fedd0e410302e
Hiromu Kamei <hk.radwimps@icloud.com>
2016-07-18 20:51:05 +0900

    [fix] Change macro to function for making hash process

--- md5_solver.c
+++ md5_solver.c
@@ -21,35 +21,29 @@
 #define C 0x98BADCFE
 #define D 0x10325476
 
+
 /*
- * Macros definition
+ * Making hash functions
  */
-#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
-
-#define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
-#define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
-#define H(x, y, z) ((x) ^ (y) ^ (z))
-#define I(x, y, z) ((y) ^ ((x) | (~z)))
-
-#define FF(a, b, c, d, x, s, t){ \
-  (a) = ((a) + F((b), (c), (d)) + (x) + (t)) & (0xFFFFFFFF); \
-  (a) = ROTATE_LEFT((a), (s)) & (0xFFFFFFFF); \
-  (a) = ((a) + (b)) & (0xFFFFFFFF); \
+void func_f(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *d, unsigned long *x, int s, unsigned long *t){
+  *a = (*a + ((*b & *c) | (~*b & *d)) + *x + *t) & 0xFFFFFFFF;
+  *a = ((*a << s) | (*a >> (32-s))) & 0xFFFFFFFF;
+  *a = (*a + *b) & 0xFFFFFFFF;
 }
-#define GG(a, b, c, d, x, s, t){ \
-  (a) = ((a) + G((b), (c), (d)) + (x) + (t)) & (0xFFFFFFFF); \
-  (a) = ROTATE_LEFT((a), (s)) & (0xFFFFFFFF); \
-  (a) = ((a) + (b)) & (0xFFFFFFFF); \
+void func_g(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *d, unsigned long *x, int s, unsigned long *t){
+  *a = (*a + ((*b & *d) | (*c & ~*d)) + *x + *t) & 0xFFFFFFFF;
+  *a = ((*a << s) | (*a >> (32-s))) & 0xFFFFFFFF;
+  *a = (*a + *b) & 0xFFFFFFFF;
 }
-#define HH(a, b, c, d, x, s, t){ \
-  (a) = ((a) + H((b), (c), (d)) + (x) + (t)) & (0xFFFFFFFF); \
-  (a) = ROTATE_LEFT((a), (s)) & (0xFFFFFFFF); \
-  (a) = ((a) + (b)) & (0xFFFFFFFF); \
+void func_h(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *d, unsigned long *x, int s, unsigned long *t){
+  *a = (*a + (*b ^ *c ^ *d) + *x + *t) & 0xFFFFFFFF;
+  *a = ((*a << s) | (*a >> (32-s))) & 0xFFFFFFFF;
+  *a = (*a + *b) & 0xFFFFFFFF;
 }
-#define II(a, b, c, d, x, s, t){ \
-  (a) = ((a) + I((b), (c), (d)) + (x) + (t)) & (0xFFFFFFFF); \
-  (a) = ROTATE_LEFT((a), (s)) & (0xFFFFFFFF); \
-  (a) = ((a) + (b)) & (0xFFFFFFFF); \
+void func_i(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *d, unsigned long *x, int s, unsigned long *t){
+  *a = (*a + (*c ^ (*b | ~*d)) + *x + *t) & 0xFFFFFFFF;
+  *a = ((*a << s) | (*a >> (32-s))) & 0xFFFFFFFF;
+  *a = (*a + *b) & 0xFFFFFFFF;
 }
 
 
@@ -119,73 +113,90 @@ int main(int argc, char **argv){
    */
   unsigned long a = A, b = B, c = C, d = D;
 
-  FF(a, b, c, d, x[ 0],  7, t[ 0]);
-  FF(d, a, b, c, x[ 1], 12, t[ 1]);
-  FF(c, d, a, b, x[ 2], 17, t[ 2]);
-  FF(b, c, d, a, x[ 3], 22, t[ 3]);
-  FF(a, b, c, d, x[ 4],  7, t[ 4]);
-  FF(d, a, b, c, x[ 5], 12, t[ 5]);
-  FF(c, d, a, b, x[ 6], 17, t[ 6]);
-  FF(b, c, d, a, x[ 7], 22, t[ 7]);
-  FF(a, b, c, d, x[ 8],  7, t[ 8]);
-  FF(d, a, b, c, x[ 9], 12, t[ 9]);
-  FF(c, d, a, b, x[10], 17, t[10]);
-  FF(b, c, d, a, x[11], 22, t[11]);
-  FF(a, b, c, d, x[12],  7, t[12]);
-  FF(d, a, b, c, x[13], 12, t[13]);
-  FF(c, d, a, b, x[14], 17, t[14]);
-  FF(b, c, d, a, x[15], 22, t[15]);
+  //FF(a, b, c, d, x[ 0],  7, t[ 0]);
+  //FF(d, a, b, c, x[ 1], 12, t[ 1]);
+  //FF(c, d, a, b, x[ 2], 17, t[ 2]);
+  //FF(b, c, d, a, x[ 3], 22, t[ 3]);
+  //FF(a, b, c, d, x[ 4],  7, t[ 4]);
+  //FF(d, a, b, c, x[ 5], 12, t[ 5]);
+  //FF(c, d, a, b, x[ 6], 17, t[ 6]);
+  //FF(b, c, d, a, x[ 7], 22, t[ 7]);
+  //FF(a, b, c, d, x[ 8],  7, t[ 8]);
+  //FF(d, a, b, c, x[ 9], 12, t[ 9]);
+  //FF(c, d, a, b, x[10], 17, t[10]);
+  //FF(b, c, d, a, x[11], 22, t[11]);
+  //FF(a, b, c, d, x[12],  7, t[12]);
+  //FF(d, a, b, c, x[13], 12, t[13]);
+  //FF(c, d, a, b, x[14], 17, t[14]);
+  //FF(b, c, d, a, x[15], 22, t[15]);
 
-  GG(a, b, c, d, x[ 1],  5, t[16]);
-  GG(d, a, b, c, x[ 6],  9, t[17]);
-  GG(c, d, a, b, x[11], 14, t[18]);
-  GG(b, c, d, a, x[ 0], 20, t[19]);
-  GG(a, b, c, d, x[ 5],  5, t[20]);
-  GG(d, a, b, c, x[10],  9, t[21]);
-  GG(c, d, a, b, x[15], 14, t[22]);
-  GG(b, c, d, a, x[ 4], 20, t[23]);
-  GG(a, b, c, d, x[ 9],  5, t[24]);
-  GG(d, a, b, c, x[14],  9, t[25]);
-  GG(c, d, a, b, x[ 3], 14, t[26]);
-  GG(b, c, d, a, x[ 8], 20, t[27]);
-  GG(a, b, c, d, x[13],  5, t[28]);
-  GG(d, a, b, c, x[ 2],  9, t[29]);
-  GG(c, d, a, b, x[ 7], 14, t[30]);
-  GG(b, c, d, a, x[12], 20, t[31]);
+  func_f(&a, &b, &c, &d, &x[ 0],  7, &t[ 0]);
+  func_f(&d, &a, &b, &c, &x[ 1], 12, &t[ 1]);
+  func_f(&c, &d, &a, &b, &x[ 2], 17, &t[ 2]);
+  func_f(&b, &c, &d, &a, &x[ 3], 22, &t[ 3]);
+  func_f(&a, &b, &c, &d, &x[ 4],  7, &t[ 4]);
+  func_f(&d, &a, &b, &c, &x[ 5], 12, &t[ 5]);
+  func_f(&c, &d, &a, &b, &x[ 6], 17, &t[ 6]);
+  func_f(&b, &c, &d, &a, &x[ 7], 22, &t[ 7]);
+  func_f(&a, &b, &c, &d, &x[ 8],  7, &t[ 8]);
+  func_f(&d, &a, &b, &c, &x[ 9], 12, &t[ 9]);
+  func_f(&c, &d, &a, &b, &x[10], 17, &t[10]);
+  func_f(&b, &c, &d, &a, &x[11], 22, &t[11]);
+  func_f(&a, &b, &c, &d, &x[12],  7, &t[12]);
+  func_f(&d, &a, &b, &c, &x[13], 12, &t[13]);
+  func_f(&c, &d, &a, &b, &x[14], 17, &t[14]);
+  func_f(&b, &c, &d, &a, &x[15], 22, &t[15]);
 
-  HH(a, b, c, d, x[ 5],  4, t[32]);
-  HH(d, a, b, c, x[ 8], 11, t[33]);
-  HH(c, d, a, b, x[11], 16, t[34]);
-  HH(b, c, d, a, x[14], 23, t[35]);
-  HH(a, b, c, d, x[ 1],  4, t[36]);
-  HH(d, a, b, c, x[ 4], 11, t[37]);
-  HH(c, d, a, b, x[ 7], 16, t[38]);
-  HH(b, c, d, a, x[10], 23, t[39]);
-  HH(a, b, c, d, x[13],  4, t[40]);
-  HH(d, a, b, c, x[ 0], 11, t[41]);
-  HH(c, d, a, b, x[ 3], 16, t[42]);
-  HH(b, c, d, a, x[ 6], 23, t[43]);
-  HH(a, b, c, d, x[ 9],  4, t[44]);
-  HH(d, a, b, c, x[12], 11, t[45]);
-  HH(c, d, a, b, x[15], 16, t[46]);
-  HH(b, c, d, a, x[ 2], 23, t[47]);
+  func_g(&a, &b, &c, &d, &x[ 1],  5, &t[16]);
+  func_g(&d, &a, &b, &c, &x[ 6],  9, &t[17]);
+  func_g(&c, &d, &a, &b, &x[11], 14, &t[18]);
+  func_g(&b, &c, &d, &a, &x[ 0], 20, &t[19]);
+  func_g(&a, &b, &c, &d, &x[ 5],  5, &t[20]);
+  func_g(&d, &a, &b, &c, &x[10],  9, &t[21]);
+  func_g(&c, &d, &a, &b, &x[15], 14, &t[22]);
+  func_g(&b, &c, &d, &a, &x[ 4], 20, &t[23]);
+  func_g(&a, &b, &c, &d, &x[ 9],  5, &t[24]);
+  func_g(&d, &a, &b, &c, &x[14],  9, &t[25]);
+  func_g(&c, &d, &a, &b, &x[ 3], 14, &t[26]);
+  func_g(&b, &c, &d, &a, &x[ 8], 20, &t[27]);
+  func_g(&a, &b, &c, &d, &x[13],  5, &t[28]);
+  func_g(&d, &a, &b, &c, &x[ 2],  9, &t[29]);
+  func_g(&c, &d, &a, &b, &x[ 7], 14, &t[30]);
+  func_g(&b, &c, &d, &a, &x[12], 20, &t[31]);
 
-  II(a, b, c, d, x[ 0],  6, t[48]);
-  II(d, a, b, c, x[ 7], 10, t[49]);
-  II(c, d, a, b, x[14], 15, t[50]);
-  II(b, c, d, a, x[ 5], 21, t[51]);
-  II(a, b, c, d, x[12],  6, t[52]);
-  II(d, a, b, c, x[ 3], 10, t[53]);
-  II(c, d, a, b, x[10], 15, t[54]);
-  II(b, c, d, a, x[ 1], 21, t[55]);
-  II(a, b, c, d, x[ 8],  6, t[56]);
-  II(d, a, b, c, x[15], 10, t[57]);
-  II(c, d, a, b, x[ 6], 15, t[58]);
-  II(b, c, d, a, x[13], 21, t[59]);
-  II(a, b, c, d, x[ 4],  6, t[60]);
-  II(d, a, b, c, x[11], 10, t[61]);
-  II(c, d, a, b, x[ 2], 15, t[62]);
-  II(b, c, d, a, x[ 9], 21, t[63]);
+  func_h(&a, &b, &c, &d, &x[ 5],  4, &t[32]);
+  func_h(&d, &a, &b, &c, &x[ 8], 11, &t[33]);
+  func_h(&c, &d, &a, &b, &x[11], 16, &t[34]);
+  func_h(&b, &c, &d, &a, &x[14], 23, &t[35]);
+  func_h(&a, &b, &c, &d, &x[ 1],  4, &t[36]);
+  func_h(&d, &a, &b, &c, &x[ 4], 11, &t[37]);
+  func_h(&c, &d, &a, &b, &x[ 7], 16, &t[38]);
+  func_h(&b, &c, &d, &a, &x[10], 23, &t[39]);
+  func_h(&a, &b, &c, &d, &x[13],  4, &t[40]);
+  func_h(&d, &a, &b, &c, &x[ 0], 11, &t[41]);
+  func_h(&c, &d, &a, &b, &x[ 3], 16, &t[42]);
+  func_h(&b, &c, &d, &a, &x[ 6], 23, &t[43]);
+  func_h(&a, &b, &c, &d, &x[ 9],  4, &t[44]);
+  func_h(&d, &a, &b, &c, &x[12], 11, &t[45]);
+  func_h(&c, &d, &a, &b, &x[15], 16, &t[46]);
+  func_h(&b, &c, &d, &a, &x[ 2], 23, &t[47]);
+
+  func_i(&a, &b, &c, &d, &x[ 0],  6, &t[48]);
+  func_i(&d, &a, &b, &c, &x[ 7], 10, &t[49]);
+  func_i(&c, &d, &a, &b, &x[14], 15, &t[50]);
+  func_i(&b, &c, &d, &a, &x[ 5], 21, &t[51]);
+  func_i(&a, &b, &c, &d, &x[12],  6, &t[52]);
+  func_i(&d, &a, &b, &c, &x[ 3], 10, &t[53]);
+  func_i(&c, &d, &a, &b, &x[10], 15, &t[54]);
+  func_i(&b, &c, &d, &a, &x[ 1], 21, &t[55]);
+  func_i(&a, &b, &c, &d, &x[ 8],  6, &t[56]);
+  func_i(&d, &a, &b, &c, &x[15], 10, &t[57]);
+  func_i(&c, &d, &a, &b, &x[ 6], 15, &t[58]);
+  func_i(&b, &c, &d, &a, &x[13], 21, &t[59]);
+  func_i(&a, &b, &c, &d, &x[ 4],  6, &t[60]);
+  func_i(&d, &a, &b, &c, &x[11], 10, &t[61]);
+  func_i(&c, &d, &a, &b, &x[ 2], 15, &t[62]);
+  func_i(&b, &c, &d, &a, &x[ 9], 21, &t[63]);
 
   a = (a + A) & 0xFFFFFFFF;
   b = (b + B) & 0xFFFFFFFF;
